e834f9505e287c4bc5188273eea67f8896d4ca03
Sangwoo (Thomas) Hahn <thahn106@gmail.com>
2022-04-05 23:36:42 +0900

    220405 Practice
    Light Practice

---
+++ By Difficulty/Bronze IV/17874.py
@@ -0,0 +1,2 @@
+n,h,v = map(int,input().split())
+print(4*max(v,n-v)*max(h,n-h))
---
+++ By Difficulty/Bronze IV/8723.py
@@ -0,0 +1,8 @@
+a = sorted(list(map(int,input().split())))
+
+if min(a)==max(a):
+    print(2)
+elif a[0]**2+a[1]**2==a[2]**2:
+    print(1)
+else:
+    print(0)
