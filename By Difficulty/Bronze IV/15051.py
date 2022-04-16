337c4dcc3d941be51798dd2fda5f8cc45a63ed76
Sangwoo (Thomas) Hahn <thahn106@gmail.com>
2022-04-16 18:18:01 +0900

    220416 Practice
    Light Practice

---
+++ By Difficulty/Bronze IV/15051.py
@@ -0,0 +1,1 @@
+a,b,c=[int(input())for i in range(3)];print(2*min(b+2*c,a+c,2*a+b))
---
+++ By Difficulty/Bronze IV/21612.py
@@ -0,0 +1,8 @@
+s = 5*int(input())-400
+print(s)
+if s < 100:
+    print(1)
+elif s > 100:
+    print(-1)
+else:
+    print(0)
